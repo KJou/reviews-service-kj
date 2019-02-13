e165b498739469b259558b49f74fb1c015765de3
KJou <joukevin@gmail.com>
2019-02-12 23:34:31 -0800

    Actually separate schema into own file
    Did not add the file correctlt to last commit...

---
+++ database/schema.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const reviewSummarySchema = new Schema({
+  productID: { type: Number, unique: true },
+  aggregateRating: Number,
+  aggregateFit: String,
+  reviews: Array,
+}, { collection: 'reviewSummaries' });
+
+const reviewSchema = new Schema({
+  rating: Number,
+  dateCreated: Date,
+  fit: String,
+  headline: String,
+  body: String,
+  username: String,
+});
+
+module.exports = {
+  ReviewSummary: mongoose.model('ReviewSummary', reviewSummarySchema),
+  Review: mongoose.model('Review', reviewSchema)
+}
