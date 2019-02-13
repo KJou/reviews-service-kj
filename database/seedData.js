ef758e72b2434217e064ba52781f48b07a95f916
KJou <joukevin@gmail.com>
2019-02-12 23:33:42 -0800

    Separate schema into own file and export

--- database/seedData.js
+++ database/seedData.js
@@ -1,43 +1,23 @@
 const faker = require('faker');
 const mongoose = require('mongoose');
 const dbConnection = require('./db.js');
+const schema = require('./schema.js');
 
-const { Schema } = mongoose;
-
-const reviewSummarySchema = new Schema({
-  productID: { type: Number, unique: true },
-  aggregateRating: Number,
-  aggregateFit: String,
-  reviews: Array,
-}, { collection: 'reviewSummaries' });
-
-const reviewSchema = new Schema({
-  rating: Number,
-  dateCreated: Date,
-  fit: String,
-  headline: String,
-  body: String,
-  username: String,
-});
-
-const ReviewSummary = mongoose.model('ReviewSummary', reviewSummarySchema);
-const Review = mongoose.model('Review', reviewSchema);
-
-const sharedProduct1 = new ReviewSummary({
+const sharedProduct1 = new schema.ReviewSummary({
   productID: 1,
   aggregateRating: 0,
   aggregateFit: 'True to size.',
   reviews: [],
 });
 
-const sharedProduct2 = new ReviewSummary({
+const sharedProduct2 = new schema.ReviewSummary({
   productID: 2,
   aggregateRating: 4.5,
   aggregateFit: '',
   reviews: [], // 2213
 });
 
-const prod3Review1 = new Review({
+const prod3Review1 = new schema.Review({
   rating: 5,
   dateCreated: faker.date.past(),
   fit: 'Runs small',
@@ -46,21 +26,21 @@ const prod3Review1 = new Review({
   username: 'BBYGirlXO',
 });
 
-const sharedProduct3 = new ReviewSummary({
+const sharedProduct3 = new schema.ReviewSummary({
   productID: 3,
   aggregateRating: 5,
   aggregateFit: 'Runs small; order next size up',
   reviews: [prod3Review1], // 1
 });
 
-const sharedProduct4 = new ReviewSummary({
+const sharedProduct4 = new schema.ReviewSummary({
   productID: 4,
   aggregateRating: 5,
   aggregateFit: '',
   reviews: [], // 2
 });
 
-const sharedProduct5 = new ReviewSummary({
+const sharedProduct5 = new schema.ReviewSummary({
   productID: 5,
   aggregateRating: 3.5,
   aggregateFit: '',
